58434cd6b57ae85e1601827aedbc50a621b7e2c5
Oleg Shatov <35371391+domwst@users.noreply.github.com>
2020-04-01 01:13:43 +0300

    Enchanced iterators and minor changes

--- hashmap.h
+++ hashmap.h
@@ -2,52 +2,159 @@
 #include <functional>
 #include <vector>
 #include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+template<class vector_iter, class list_iter>
+class base_hashmap_iterator {
+private:
+    vector_iter cur, end;
+    list_iter biter;
+
+    void push() {
+        while (cur != end && biter == cur->end()) {
+            ++cur;
+            if (cur != end) {
+                biter = cur->begin();
+            }
+        }
+    }
+
+public:
+    base_hashmap_iterator() = default;
+    base_hashmap_iterator(const base_hashmap_iterator &it) = default;
+
+    base_hashmap_iterator(const vector_iter &_cur, const list_iter &_biter, const vector_iter &_end) :
+        cur(_cur), end(_end), biter(_biter) {}
+
+    template<class list_type>
+    base_hashmap_iterator(std::vector<list_type> &buckets, bool back = false) :
+        cur(buckets.begin()), end(buckets.end()), biter(cur->begin()) {
+        
+        if (!back) {
+            push();
+        } else {
+            cur = end = buckets.end();
+            biter = buckets.back().end();
+        }
+    }
+
+    template<class list_type>
+    base_hashmap_iterator(const std::vector<list_type> &buckets, bool back = false) :
+        cur(buckets.begin()), end(buckets.end()), biter(cur->begin()) {
+        
+        if (!back) {
+            push();
+        } else {
+            cur = end = buckets.end();
+            biter = buckets.back().end();
+        }
+    }
+
+    void bucket_advance(size_t l) {
+        cur += l;
+    }
+
+    base_hashmap_iterator& operator++() {
+        ++biter;
+        push();
+        return *this;
+    }
+
+    base_hashmap_iterator operator++(int) {
+        base_hashmap_iterator cp(*this);
+        ++(*this);
+        return cp;
+    }
+
+    base_hashmap_iterator& operator=(const base_hashmap_iterator &iter) {
+        cur = iter.cur;
+        end = iter.end;
+        biter = iter.biter;
+        return *this;
+    }
+
+    typename list_iter::value_type operator*() {
+        return *biter;
+    }
+
+    list_iter operator->() {
+        return biter;
+    }
+
+    bool operator==(const base_hashmap_iterator &it) {
+        return it.cur == cur && it.biter == biter;
+    }
+
+    bool operator!=(const base_hashmap_iterator &it) {
+        return !(*this == it);
+    }
+};
 
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType>>
 class HashMap {
 private:
-    static const size_t sizes[];
+    static constexpr size_t sizes[] = {
+    5, 11, 23, 47, 97, 197, 397, 797, 1597, 3203, 6421,
+    12853, 25717, 51437, 102877, 205759, 411527, 823117,
+    1646237, 3292489, 6584983, 13169977, 26339969, 52679969};
+    static constexpr size_t growLoadFactorThreshold = 130, shrintLoadFactorThreshold = 30;
 
-    size_t cur_size = 0, cur_len = 0;
-    std::vector<std::forward_list<std::pair<const KeyType, ValueType>>> buckets;
+    using stored_type = std::pair<const KeyType, ValueType>;
+    using list_type = std::forward_list<stored_type>;
+    using vector_type = std::vector<list_type>;
+
+    size_t cur_size = 0, cur_capacity = 0;
+    vector_type buckets;
     Hash hasher;
 
-    void change_size(size_t new_sz) {
-        if (new_sz == cur_len) {
+    void change_size(size_t new_capacity) {
+        if (new_capacity == cur_capacity) {
             return;
         }
-        new_sz = sizes[cur_len = new_sz];
-        std::vector<std::forward_list<std::pair<const KeyType, ValueType>>> new_buckets(new_sz);
+        cur_capacity = new_capacity;
+        new_capacity = sizes[cur_capacity];
+        vector_type new_buckets(new_capacity);
         for (auto &b : buckets) {
             for (auto &x : b) {
-                new_buckets[hasher(x.first) % new_sz].push_front(std::move(x));
+                new_buckets[hasher(x.first) % new_capacity].push_front(std::move(x));
             }
         }
         buckets.swap(new_buckets);
     }
 
-    size_t load_factor() const {
-        return cur_size * 100 / sizes[cur_len];
+    size_t load_factor() const {  // returns cur_size / cur_capacity in percents. i.e. returns load factor in percents
+        return cur_size * 100 / sizes[cur_capacity];
     }
 
     bool trySizeUp() {
-        if (load_factor() > 130) {
-            change_size(cur_len + 1);
+        if (load_factor() > growLoadFactorThreshold) {
+            change_size(cur_capacity + 1);
             return true;
         }
         return false;
     }
 
     bool trySizeDown() {
-        if (load_factor() < 30 && cur_len) {
-            change_size(cur_len - 1);
+        if (load_factor() < shrintLoadFactorThreshold && cur_capacity > 0) {
+            change_size(cur_capacity - 1);
             return true;
         }
         return false;
     }
 
+    std::pair<size_t, size_t> base_find(const KeyType &val) const {
+        size_t h = hasher(val) % buckets.size();
+        for (auto it = buckets[h].begin(), cur = 0ull; it != buckets[h].end(); ++it, ++cur) {
+            if (it->first == val) {
+                return std::make_pair(h, (size_t)cur);
+            }
+        }
+        return std::make_pair(buckets.size(), std::distance(buckets.back().begin(), buckets.back().end()));
+    }
+
 public:
-    HashMap(const Hash &Hasher = Hash()) : buckets(sizes[0]), hasher(Hasher) {}
+    explicit HashMap(const Hash &Hasher = Hash()) : buckets(sizes[0]), hasher(Hasher) {}
 
     template<class Iter>
     HashMap(Iter beg, Iter end, const Hash &Hasher = Hash()) : HashMap(Hasher) {
@@ -56,9 +163,15 @@ public:
         }
     }
 
-    HashMap(const std::initializer_list<std::pair<const KeyType, ValueType>> &l, const Hash &Hasher = Hash()) : HashMap(l.begin(), l.end(), Hasher) {}
+    HashMap(const std::initializer_list<stored_type> &l, const Hash &Hasher = Hash()) :
+        HashMap(l.begin(), l.end(), Hasher) {}
 
-    HashMap(const std::vector<std::pair<const KeyType, ValueType>> &vals, const Hash &Hasher = Hash()) : HashMap(vals.begin(), vals.end(), Hasher) {}
+    HashMap(const std::vector<stored_type> &vals, const Hash &Hasher = Hash()) :
+        HashMap(vals.begin(), vals.end(), Hasher) {}
+
+    using iterator = base_hashmap_iterator<typename vector_type::iterator, typename list_type::iterator>;
+    using const_iterator = base_hashmap_iterator<typename vector_type::const_iterator,
+                                                 typename list_type::const_iterator>;
 
     size_t size() const {
         return cur_size;
@@ -82,7 +195,7 @@ public:
         return 0;
     }
 
-    void insert(const std::pair<KeyType, ValueType> &val) {
+    void insert(const stored_type &val) {
         if (count(val.first))
             return;
         buckets[hasher(val.first) % buckets.size()].push_front(val);
@@ -103,13 +216,13 @@ public:
     }
 
     ValueType& operator[](const KeyType &key) {
-        size_t h = hasher(key) % sizes[cur_len];
+        size_t h = hasher(key) % sizes[cur_capacity];
         for (auto &[x, y] : buckets[h]) {
             if (x == key) {
                 return y;
             }
         }
-        buckets[h].push_front({key, ValueType()});
+        buckets[h].emplace_front(key, ValueType());
         ++cur_size;
         if (trySizeUp()) {
             return operator[](key);
@@ -128,180 +241,46 @@ public:
     }
 
     void clear() {
-        for (auto &b : buckets) {
-            b.clear();
+        for (auto &bucket : buckets) {
+            bucket.clear();
         }
         cur_size = 0;
         change_size(0);
     }
 
-    class const_iterator {
-        friend HashMap;
-    private:
-        typename std::vector<std::forward_list<std::pair<const KeyType, ValueType>>>::const_iterator cur, end;
-        typename std::forward_list<std::pair<const KeyType, ValueType>>::const_iterator biter;
-
-        void push() {
-            while (cur != end && biter == cur->end()) {
-                ++cur;
-                if (cur != end) {
-                    biter = cur->begin();
-                }
-            }
-        }
-
-    public:
-        const_iterator() {}
-        const_iterator(const HashMap &mp)  : cur(mp.buckets.begin()), end(mp.buckets.end()), biter(cur->begin()) {}
-        const_iterator(const const_iterator &it) : cur(it.cur), end(it.end), biter(it.biter) {}
-
-        const_iterator& operator++() {
-            ++biter;
-            push();
-            return *this;
-        }
-
-        const_iterator operator++(int) {
-            const_iterator cp(*this);
-            ++(*this);
-            return cp;
-        }
-
-        const_iterator& operator=(const const_iterator &iter) {
-            cur = iter.cur;
-            end = iter.end;
-            biter = iter.biter;
-            return *this;
-        }
-
-        const std::pair<const KeyType, ValueType>& operator*() {
-            return *biter;
-        }
-
-        typename std::forward_list<std::pair<const KeyType, ValueType>>::const_iterator operator->() {
-            return biter;
-        }
-
-        bool operator==(const const_iterator &it) {
-            return it.cur == cur && it.biter == biter;
-        }
-
-        bool operator!=(const const_iterator &it) {
-            return !(*this == it);
-        }
-    };
-
-    class iterator {
-        friend HashMap;
-    private:
-        typename std::vector<std::forward_list<std::pair<const KeyType, ValueType>>>::iterator cur, end;
-        typename std::forward_list<std::pair<const KeyType, ValueType>>::iterator biter;
-
-        void push() {
-            while (cur != end && biter == cur->end()) {
-                ++cur;
-                if (cur != end) {
-                    biter = cur->begin();
-                }
-            }
-        }
-
-    public:
-        iterator() {}
-        iterator(HashMap &mp)  : cur(mp.buckets.begin()), end(mp.buckets.end()), biter(cur->begin()) {}
-        iterator(const iterator &it) : cur(it.cur), end(it.end), biter(it.biter) {}
-
-        iterator& operator++() {
-            ++biter;
-            push();
-            return *this;
-        }
-
-        iterator operator++(int) {
-            iterator cp(*this);
-            ++(*this);
-            return cp;
-        }
-
-        iterator& operator=(const iterator &iter) {
-            cur = iter.cur;
-            end = iter.end;
-            biter = iter.biter;
-            return *this;
-        }
-
-        const std::pair<const KeyType, ValueType>& operator*() {
-            return *biter;
-        }
-
-        typename std::forward_list<std::pair<const KeyType, ValueType>>::iterator operator->() {
-            return biter;
-        }
-
-        bool operator==(const iterator &it) {
-            return it.cur == cur && it.biter == biter;
-        }
-
-        bool operator!=(const iterator &it) {
-            return !(*this == it);
-        }
-    };
-
     const_iterator find(const KeyType &val) const {
-        size_t h = hasher(val) % buckets.size();
-        for (auto it = buckets[h].begin(); it != buckets[h].end(); ++it) {
-            if (it->first == val) {
-                const_iterator iter(*this);
-                iter.cur = buckets.begin() + h;
-                iter.biter = it;
-                return iter;
-            }
-        }
-        return end();
+        auto [block_idx, item_idx] = base_find(val);
+        auto item_iterator = buckets[std::min(block_idx, buckets.size() - 1)].begin();  // For case of end iterator
+        std::advance(item_iterator, item_idx);
+        return const_iterator(buckets.begin() + block_idx,
+                              item_iterator, buckets.end());
     }
 
     iterator find(const KeyType &val) {
-        size_t h = hasher(val) % buckets.size();
-        for (auto it = buckets[h].begin(); it != buckets[h].end(); ++it) {
-            if (it->first == val) {
-                iterator iter(*this);
-                iter.cur = buckets.begin() + h;
-                iter.biter = it;
-                return iter;
-            }
-        }
-        return end();
+        auto [block_idx, item_idx] = base_find(val);
+        auto item_iterator = buckets[std::min(block_idx, buckets.size() - 1)].begin();
+        std::advance(item_iterator, item_idx);
+        return iterator(buckets.begin() + block_idx,
+                        item_iterator, buckets.end());
     }
 
      iterator begin() {
-        iterator iter(*this);
-        iter.push();
+        iterator iter(buckets);
         return iter;
      }
 
      iterator end() {
-        iterator iter(*this);
-        iter.cur = iter.end;
-        iter.biter = buckets.back().end();
+        iterator iter(buckets, true);
         return iter;
      }
 
      const_iterator begin() const {
-        const_iterator iter(*this);
-        iter.push();
+        const_iterator iter(buckets);
         return iter;
      }
 
      const_iterator end() const {
-        const_iterator iter(*this);
-        iter.cur = iter.end;
-        iter.biter = buckets.back().end();
+        const_iterator iter(buckets, true);
         return iter;
      }
 };
-
-template<class KeyType, class ValueType, class Hash>
-const size_t HashMap<KeyType, ValueType, Hash>::sizes[] = {
-    5, 11, 23, 47, 97, 197, 397, 797, 1597, 3203, 6421,
-    12853, 25717, 51437, 102877, 205759, 411527, 823117,
-    1646237, 3292489, 6584983, 13169977, 26339969, 52679969};
